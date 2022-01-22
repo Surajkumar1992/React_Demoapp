fe9a1df390ed4c6696ff8c5c8cd812d453ffc34d
SurajKumar1992 <suraj.subham@gmail.com>
2022-01-23 00:34:46 +0530

    Initial Commit

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+import classes from './User.module.css';
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+
+// const User = (props) => {
+//   return <li className={classes.user}>{this.props.name}</li>;
+// };
+
+export default User;
